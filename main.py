a5423b88dc6220b8f7d8febdbbcdbfd9b91bb7db
raiha-4368 <kuriharay0106@gmail.com>
2026-03-14 11:11:45 +0900

    [UPDATE] 色を変更

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ class CounterApp:
     # -------------------------
     def __init__(self, root):
         self.root = root
+        self.root.configure(bg="#fffacd")
         self.count = 0
         self.root.title("Counter App")
         self.root.geometry("300x300")
         # -------------------------
         # フレーム生成
         # -------------------------
-        self.main_frame = tk.Frame(self.root)
+        self.main_frame = tk.Frame(self.root, bg="#fffacd")
         self.main_frame.pack()
 
         #初期表示
@@ -40,19 +41,19 @@ class CounterApp:
         # mainフレーム内の要素
         # -------------------------
         # カウント表示用ラベル
-        self.display_count = tk.Label(self.main_frame, text='0', font=('Arial', 40))
+        self.display_count = tk.Label(self.main_frame, text='0', font=('Arial', 40),bg="#fffacd")
         self.display_count.pack()
 
         #加算用ボタン
-        btn_plus = tk.Button(self.main_frame, text="+1", command=self.plus)
+        btn_plus = tk.Button(self.main_frame, text="+1", command=self.plus, font=('Arial', 20), bg="#fa8072", fg="#696969")
         btn_plus.pack(side="left", padx=(10,10))
 
         #減算用ボタン
-        btn_minus = tk.Button(self.main_frame, text="-1", command=self.minus)
+        btn_minus = tk.Button(self.main_frame, text="-1", command=self.minus, font=('Arial', 20), bg="#add8e6", fg="#696969")
         btn_minus.pack(side="left", padx=(10,10))
 
         #reset用ボタン
-        btn_reset = tk.Button(self.main_frame, text="Reset", command=self.reset)
+        btn_reset = tk.Button(self.main_frame, text="Reset", command=self.reset, font=('Arial', 20), bg="#98fb98", fg="#696969")
         btn_reset.pack(side="left", padx=(10,10))
 
     def plus(self):
